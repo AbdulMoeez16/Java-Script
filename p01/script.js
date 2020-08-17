a41e08d3e34b65e513898fdda338303e9db80248
AbdulMoeez16 <69387585+AbdulMoeez16@users.noreply.github.com>
2020-08-17 18:47:14 +0500

    Project completed
    Added all the thingsd to validate the four values

--- p01/script.js
+++ p01/script.js
@@ -20,10 +20,14 @@ function showSuccess (input) {
 }
 
 // Function to check if email is valid
-function isValidEmail (email) {
+function checkEmail (input) {
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-}
+        if ( re.test(input.value.trim())  ){
+            showSuccess (input);
+        } else {
+            showError (input, `This is not a valid email` );
+        }
+}    
 
 // Function to check if required fields have data 
 function checkRequired (inputArray) {
@@ -37,6 +41,25 @@ function checkRequired (inputArray) {
     });
 }
 
+//Function to checkLength
+function checkLength (input, min, max){
+    if (input.value.length < min) {
+        showError (input,`${getFieldId(input)} needs to be atleast ${min} characters`)
+    } else if (input.value.length > max) {
+        showError (input,`${getFieldId(input)} needs to be less than ${max} characters`)
+    } else {
+        showSuccess (input)
+    }
+}
+
+//Function for matching passwords
+function CheckPasswordsMatch (input1, input2) {
+    if (input1.value !== input2.value) {
+        showError (input2, 'Passwords do not match')
+    }
+}
+
+
 // Function to get id of the input field with proper syntax
 function getFieldId (input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -47,5 +70,9 @@ form.addEventListener('submit',function(e) {
     e.preventDefault (); 
     
     checkRequired ([username,email,password,password2]);
+    checkLength (username,3,10);
+    checkLength (password,6,30);
+    checkEmail (email);
+    CheckPasswordsMatch (password,password2)
 })    
 
