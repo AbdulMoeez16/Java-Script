4297b156e82fe5420678edecc02825dbf8fb522d
AbdulMoeez16 <69387585+AbdulMoeez16@users.noreply.github.com>
2020-08-23 12:36:59 +0500

    Update spript.js

--- p03/spript.js
+++ p03/spript.js
@@ -2,7 +2,7 @@ const video = document.getElementById ('video');
 const play = document.getElementById ('play');
 const stop = document.getElementById ('stop');
 const progress = document.getElementById ('progress');
-const timestamp = document.getElementById ('timestap');
+const timestamp = document.getElementById ('timestamp');
 
 //Functions
 // function to toggle video (play or pause)
